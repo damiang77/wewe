a69c6ca79b4fd7d7bc57ddfbf6c326724de7f7f1
Damian Garbera <damiangarbera@gmail.com>
2020-07-09 22:11:45 +0200

    change to REST

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+
 
 function App() {
+
+  const [videos, setVideos] = useState([]);
+
+  const apiUrl = "https://api.gar.ovh"  
+
+  const fullUrl = (shortUrl) => {
+    return `${apiUrl}${shortUrl}`
+  }
+  useEffect(()=>[
+    axios.get('https://api.gar.ovh/videos')
+  .then(function (response) {
+    setVideos(response.data)
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  })
+  ], [])
+
+  const style = {
+    width: "150px",
+  };
+
+
   return (
     <div className="App">
-     <h5>Hołk</h5>
+      {videos.map((x)=>{
+        return (<p>
+          {fullUrl(x.photo.url)}
+          <img src={fullUrl(x.photo.url)}/>
+          </p>)
+      })}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { GraphQLClient, ClientContext } from "graphql-hooks";
+
+const client = new GraphQLClient({
+  url: "https://graphql.datocms.com/",
+  headers: {
+    Authorization: "Bearer c206af7707fc2732c79e3bc1fcd6e6",
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ClientContext.Provider value={client}>
+      <App />
+    </ClientContext.Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
