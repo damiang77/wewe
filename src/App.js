9d087c08d2208f5db80f9d9977464ea62d6adff4
Damian Garbera <damiangarbera@gmail.com>
2020-07-12 18:12:36 +0200

    api change

--- src/App.js
+++ src/App.js
@@ -2,43 +2,52 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 
-
 function App() {
-
   const [videos, setVideos] = useState([]);
 
-  const apiUrl = "https://api.gar.ovh"  
+  const apiUrl = "https://api.gar.ovh";
 
   const fullUrl = (shortUrl) => {
-    return `${apiUrl}${shortUrl}`
-  }
-  useEffect(()=>[
-    axios.get('https://api.gar.ovh/videos')
-  .then(function (response) {
-    setVideos(response.data)
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  })
-  ], [])
+    return `${apiUrl}${shortUrl}`;
+  };
+
+  
+  useEffect(
+    () => {
+      axios
+        .get("https://api.gar.ovh/videos")
+        .then(function (response) {
+          setVideos(response.data);
+          console.log(response);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        })
+        .finally(function () {
+          // always executed
+        });
+
+        return ()=>{
+          setVideos([])
+        }
+      },
+    []
+  );
 
   const style = {
     width: "150px",
   };
 
-
   return (
     <div className="App">
-      {videos.map((x)=>{
-        return (<p>
-          {fullUrl(x.photo.url)}
-          <img src={fullUrl(x.photo.url)}/>
-          </p>)
+      {videos.map((x) => {
+        return (
+          <p>
+            {fullUrl(x.photo.url)}
+            <img src={fullUrl(x.photo.url)} />
+          </p>
+        );
       })}
     </div>
   );
